991b8b5e4ff9408fd6c4e40f72304f3c375fbcd0
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-02-24 01:23:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace Askerlik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yasinizi giriniz: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Okuyor musunuz? (evet/hayir): ");
+            string okul = Console.ReadLine().ToLower();
+
+            if (yas > 18)
+            {
+                if (okul == "evet")
+                {
+                    Console.WriteLine("Askerlige cagrilmaz.");
+                }
+                else if (okul == "hayir")
+                {
+                    Console.WriteLine("Askerlige cagrilabilir");
+                }
+                else
+                {
+                    Console.WriteLine("Hatali yazim");
+                }
+            }
+            else
+            { 
+                Console.WriteLine("Askerlige cagrilmaz");
+            }
+        }
+    }
+}
